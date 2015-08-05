7ac320cad9ebc54a570606d7ce3e3bd8725f9b5c
Jose Antonio Rodriguez <rodriguezgomez.jose@gmail.com>
2015-08-05 17:17:39 +0200

    Autorizacion

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,8 +1,8 @@
-/* MW de autorizacion de accesos HTTP restringidos
+// MW de autorizacion de accesos HTTP restringidos
 exports.loginRequired = function(req, res, next) {
     if(req.session.user) { next(); }
     else { res.redirect('/login'); }
-}; */
+}; 
 
 // GET /login -- Formulario de login
 exports.new = function(req, res) {
